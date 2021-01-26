ee3898a25360c2801caf0a0aef2077f3e4ab0846
Peter Feldens <peter.feldens@io-warnemuende.de>
2021-01-26 20:41:05 +0100

    first try to calculate nadir and work on subimages without tiling for boulder detection

--- apply_object_detect_config_file.py
+++ apply_object_detect_config_file.py
@@ -3,11 +3,11 @@ import os
 #main1
 #image_folder = '/media/peter/Daten_1/BalticSeaTraining/tifs/tiles/'
 #image_type = '.tif'
+#model_path = os.path.join('./resnet50_csv_SR100_17_r09.h5')
 
 #all
 output = './output/AdlerGrund_Test1.csv'
 labels_to_names = {0: 'stone'}
-model_path = os.path.join('./resnet50_csv_SR100_17_r09.h5')
 convert_model = 'yes'
 detection_threshold = 0.2  #include detections with accuracy above
 min_side=800
